f84cb4e606c8709a99624d530fdc64ac272717fc
jurest <jure.stepisnik@smartis.si>
2019-03-10 20:37:43 +0100

    Added caching and subscribe route

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const app = require('express')();
-const http = require('http').Server(app);
-var Twitter = require('twitter');
+const Twitter = require('twitter');
+const bodyParser = require('body-parser')
+const { setCache, getCache, getKeys } = require('./src/utils/redis.helper');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+const init = async () => {
+    await setCache('la', 123)
+    await setCache('la1', 1234)
+    await setCache('la2', 12345)
+    const keys = await getCache('la3');
+
+    console.log(keys)
+};
+
+//init();
 
 const client = new Twitter({
     consumer_key: 'gbveCqmu3utJ0ouDQczVAG1S8',
@@ -9,20 +25,42 @@ const client = new Twitter({
     access_token_secret: 'z1qSgEHUnEyf8ACC8fwq1ZReTTfQCXU1jByoKisiTR6Ho'
 });
 
-const stream = client.stream('statuses/filter', {track: 'javascript'});
+const stream = client.stream('statuses/filter', { track: 'javascript' });
 
 stream.on('data', (event) => {
-    console.log(event)
+    //console.log(event)
 });
 
 stream.on('error', (error) => {
     console.log(error)
 })
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {    
     res.send('hello world this is it');
 });
 
-http.listen(4040, () => {
+app.post('/subscribe', async (req, res) => {
+    try {
+        const { url } = req.body;
+
+        console.log(url)
+
+        const cachedValue = await getCache(url);
+
+        if (cachedValue) {
+            return res.status(302).send();
+        }
+
+        await setCache(url);
+
+        return res.send();
+    } catch (err) {
+        console.log(err);
+
+        return res.status(500).send(`Error subscribing to channel.`);
+    }
+});
+
+app.listen(4040, () => {
     console.log(`listening on port: 4040`);
 });
